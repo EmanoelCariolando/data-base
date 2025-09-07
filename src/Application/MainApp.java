134246aeab1e11392df20ffabece3721b9d16664
Emanoel Cariolando <136757338+EmanoelCariolando@users.noreply.github.com>
2025-09-07 16:14:53 -0300

    Data base - update data

--- src/Application/MainApp.java
+++ src/Application/MainApp.java
@@ -1,11 +1,9 @@
 package Application;
 import db.DB;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
 
@@ -13,44 +11,31 @@ public class MainApp {
     public static void main(String[] args) throws ParseException {
 
         Scanner sc = new Scanner(System.in);
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        String name = "";
-        String email = "";
-        String birthDate;
         double baseSalary;
         int departmentId;
 
         Connection conn = null;
         PreparedStatement ps = null;
 
-            System.out.print("Type Your Name: ");
-            name = sc.nextLine();
-            System.out.print("Type Your Email: ");
-            email = sc.nextLine();
-            System.out.print("Type your birthdate (dd/mm/yyyy): ");
-            birthDate = sc.nextLine();
-            System.out.print("Type your Salary: ");
-            baseSalary = sc.nextDouble();
-            System.out.print("Type your departmentId (from 1 to 4): ");
-            departmentId = sc.nextInt();
-
+        System.out.print("Update your Base Salary: ");
+        baseSalary = sc.nextDouble();
+        System.out.print("Type your departmentId (from 1 to 4): ");
+        departmentId = sc.nextInt();
 
         try{
             conn = DB.getConn();
 
             ps = conn.prepareStatement(
-                    "INSERT INTO seller"
-                    + "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
-                    + "VALUES "
-                    + "(?,?,?,?,?)");
-                   ps.setString(1, name);
-                   ps.setString(2,email);
-                   ps.setDate(3, new java.sql.Date(sdf.parse(birthDate).getTime()));
-                   ps.setDouble(4, baseSalary);
-                   ps.setInt(5, departmentId);
+                    "UPDATE seller "
+                            + "SET BaseSalary = BaseSalary + ? "
+                            + "WHERE "
+                            + "(DepartmentId = ?)");
 
-                   int rows = ps.executeUpdate();
-                   System.out.println("Done! Rows Affected: " + rows);
+            ps.setDouble(1, baseSalary);
+            ps.setInt(2, departmentId);
+            int rows = ps.executeUpdate();
+            System.out.println("Done!");
+            System.out.println("Total Rows Affected: " + rows);
 
         }
         catch (SQLException e) {
@@ -61,4 +46,5 @@ public class MainApp {
         }
 
     }
-    }
+}
+
